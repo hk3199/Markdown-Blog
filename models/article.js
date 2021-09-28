c09e79e9335c90cd8ecfc1d7579be6007a5536ca
Himanshu Kumar <50806321+hk3199@users.noreply.github.com>
2021-09-28 18:11:23 +0530

    m

--- models/article.js
+++ models/article.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose')
 const marked= require('marked')
 const slugify= require('slugify')
-const createDomPurifier = require('dompurifier')
+const createDomPurify = require('dompurify')
 const {JSDOM}= require ('jsdom')
 const dompurify= createDomPurify(new JSDOM().window)
 const articleSchema = new mongoose.Schema({
@@ -25,6 +25,10 @@ const articleSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
+      },
+      sanitizedHtml:{
+          type: String,
+          required: true
       }
 })
 
@@ -32,7 +36,9 @@ articleSchema.pre('validate', function(next) {
     if (this.title) {
       this.slug = slugify(this.title, { lower: true, strict: true })
     }
-  
+    if(this.markdown){
+        this.sanitizedHtml= dompurify.sanitize(marked(this.markdown))
+    }
   
     next()
   })
